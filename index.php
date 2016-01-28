83643ca00df49c3fe36c53816a72aa56c2b39c03
kenkoooo <kenkou.n@gmail.com>
2016-01-28 17:41:13 +0900

    Add memory column

--- index.php
+++ index.php
@@ -6,29 +6,34 @@ $pbsnodes = [];
 $job_counts = [];
 $job_threads = [];
 foreach ($pbsnodes_xml->Node as $value) {
-    $node_name = (string)$value->name;
-    array_push($pbsnodes, $value);
-    if (isset($value->jobs)) {
-        $job_counts[$node_name] = substr_count($value->jobs, ',') + 1;
-    }
-    else {
-        $job_counts[$node_name] = 0;
-    }
-    $job_threads[$node_name] = (int)$value->np;
+	$node_name = (string) $value->name;
+	array_push($pbsnodes, $value);
+	if (isset($value->jobs)) {
+		$job_counts[$node_name] = substr_count($value->jobs, ',') + 1;
+	} else {
+		$job_counts[$node_name] = 0;
+	}
+	$job_threads[$node_name] = (int) $value->np;
 }
 
 $node_job_counts = [];
 $owners = [];
 foreach ($qstat_xml->Job as $value) {
-    if ($value->job_state != 'R') continue;
-    $exec_host = explode("+", (string)$value->exec_host);
-    $owner = explode("@", (string)$value->Job_Owner) [0];
-    $owners[] = $owner;
-    foreach ($exec_host as $host) {
-        $node_name = explode("/", $host) [0];
-        if (!isset($node_job_counts[$owner][$node_name])) $node_job_counts[$owner][$node_name] = 0;
-        $node_job_counts[$owner][$node_name]++;
-    }
+	if ($value->job_state != 'R') {
+		continue;
+	}
+
+	$exec_host = explode("+", (string) $value->exec_host);
+	$owner = explode("@", (string) $value->Job_Owner)[0];
+	$owners[] = $owner;
+	foreach ($exec_host as $host) {
+		$node_name = explode("/", $host)[0];
+		if (!isset($node_job_counts[$owner][$node_name])) {
+			$node_job_counts[$owner][$node_name] = 0;
+		}
+
+		$node_job_counts[$owner][$node_name]++;
+	}
 }
 
 $owners = array_unique($owners);
@@ -37,8 +42,8 @@ shuffle($colors);
 $k = 0;
 $palette = [];
 foreach ($owners as $owner) {
-    $palette[$owner] = $colors[$k];
-    $k = ($k + 1) % sizeof($colors);
+	$palette[$owner] = $colors[$k];
+	$k = ($k + 1) % sizeof($colors);
 }
-include ('html.inc');
+include 'html.inc';
 ?>
